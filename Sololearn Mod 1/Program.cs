ea458cbe921de2dcd4b9b19551bf5e11250baf54
Gaven Rudiak <gavenrudiak@outlook.com>
2020-09-01 08:04:55 -0400

    Add project files.

---
+++ Sololearn Mod 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Sololearn_Mod_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int myAge;
+            myAge = 18;
+
+            int x = 42;
+            double pi = 3.14;
+            char y = 'Z';
+            bool isOnline = true;
+            string firstName = "David";
+        }
+    }
+}
