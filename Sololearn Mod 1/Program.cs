f7a0fe66dbaabbb4cd77d7f1b9802d21681c02d9
Gaven Rudiak <gavenrudiak@outlook.com>
2020-09-01 08:16:06 -0400

    "Getting User Input"

--- Sololearn Mod 1/Program.cs
+++ Sololearn Mod 1/Program.cs
@@ -43,6 +43,21 @@ namespace Sololearn_Mod_1
             }
             // Output: x = 10; y = 20
 
+            static void Main(string[] args)
+            {
+                string yourName;
+                Console.WriteLine("What is your name?");
+
+                yourName = Console.ReadLine();
+
+                Console.WriteLine("Hello {0}", yourName);
+            }
+
+            static void Main(string[] args)
+            {
+                int age = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("You are {0} years old", age);
+            }
 
 namespace SoloLearn
     {
